450d91e87138ac80b2dd3dee3963b4ce9e545ec6
samolevich <samolevich@gmail.com>
2020-06-17 16:35:19 +0300

    add css main colors and font

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import style from './App.module.css';
 
 function App() {
-  return <div>x</div>;
+  return <div className={style}>location skills projects history contact education about sertificate есть еще-коечто!
+  <img src="img/about.svg" alt="icon" />
+  <img src="img/certificate.svg" alt="icon" />
+  <img src="img/contacts.svg" alt="icon" />
+  <img src="img/education.svg" alt="icon" />
+  <img src="img/expirience.svg" alt="icon" />
+  <img src="img/location.svg" alt="icon" />
+  <img src="img/projects.svg" alt="icon" />
+  <img src="img/skills.svg" alt="icon" />
+  <img src="img/wink.svg" alt="icon" />
+  </div>;
 }
 
 export default App;
