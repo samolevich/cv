158a50b707d9f6690b26652f9cacbde0657ab1a6
samolevich <samolevich@gmail.com>
2020-06-18 14:13:08 +0300

    add normalize css

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import style from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={style.content}>
+      <div>x</div>
+      <div>location skills projects history contact education about sertificate есть еще-коечто!https://api.github.com/users/samolevich
+        <img src="img/about.svg" alt="icon" />
+        <img src="img/certificate.svg" alt="icon" />
+        <img src="img/contacts.svg" alt="icon" />
+        <img src="img/education.svg" alt="icon" />
+        <img src="img/expirience.svg" alt="icon" />
+        <img src="img/location.svg" alt="icon" />
+        <img src="img/projects.svg" alt="icon" />
+        <img src="img/skills.svg" alt="icon" />
+        <img src="img/wink.svg" alt="icon" />
+      </div>
+      <div>x</div>
+    </div>
+  );
+}
+
+export default App;
