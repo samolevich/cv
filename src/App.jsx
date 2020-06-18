e8d62d927b22f8463b704d73689c15b1d37c985b
samolevich <samolevich@gmail.com>
2020-06-18 14:57:33 +0300

    set layout with css grid

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,34 @@ import style from './App.module.css';
 
 const App = () => {
   return (
-    <div className={style.content}>
-      <div>x</div>
-      <div>location skills projects history contact education about sertificate есть еще-коечто!https://api.github.com/users/samolevich
-        <img src="img/about.svg" alt="icon" />
-        <img src="img/certificate.svg" alt="icon" />
-        <img src="img/contacts.svg" alt="icon" />
-        <img src="img/education.svg" alt="icon" />
-        <img src="img/expirience.svg" alt="icon" />
-        <img src="img/location.svg" alt="icon" />
-        <img src="img/projects.svg" alt="icon" />
-        <img src="img/skills.svg" alt="icon" />
-        <img src="img/wink.svg" alt="icon" />
+    <div className={style.main}>
+      <div></div>
+      <div className={style.content}>
+        <div className={style.bar}>bar</div>
+        <div className={style.info}>info</div>
       </div>
-      <div>x</div>
+      <div></div>
     </div>
   );
 }
 
 export default App;
+
+// location
+// skills
+// projects
+// history
+// contact
+// education
+// about
+// sertificate
+// https://api.github.com/users/samolevich
+// <img src="img/about.svg" alt="icon" />
+// <img src="img/certificate.svg" alt="icon" />
+// <img src="img/contacts.svg" alt="icon" />
+// <img src="img/education.svg" alt="icon" />
+// <img src="img/expirience.svg" alt="icon" />
+// <img src="img/location.svg" alt="icon" />
+// <img src="img/projects.svg" alt="icon" />
+// <img src="img/skills.svg" alt="icon" />
+// <img src="img/wink.svg" alt="icon" />
