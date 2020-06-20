c8f3e6d45c124b12b09c545a689466c2131d8b0f
samolevich <samolevich@gmail.com>
2020-06-21 01:08:29 +0300

    Updates

---
+++ precache-manifest.b3742c603ffd5f13958be0b646e85689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fca7ed646f3c6ffd1f87cc53198b467",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "8a609fdd41e8a4a1cd09",
+    "url": "/cv/static/css/main.f65ffac9.chunk.css"
+  },
+  {
+    "revision": "5d2af1912bda44144e4f",
+    "url": "/cv/static/js/2.15dea589.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cv/static/js/2.15dea589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a609fdd41e8a4a1cd09",
+    "url": "/cv/static/js/main.5ccaa53d.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  }
+]);
