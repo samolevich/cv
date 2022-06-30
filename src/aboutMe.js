379e1fff6396a2677f5c9d697155efe35badb573
Konstantin Samolevich <42073618+samolevich@users.noreply.github.com>
2022-06-30 09:31:26 +0300

    start to fill info in separate file

---
+++ src/aboutMe.js
@@ -0,0 +1,54 @@
+export default {
+  skills: [
+    "Javascript",
+    "React",
+    "Redux (Thunk)",
+    "NodeJS",
+    "Express",
+    "MongoDB",
+    "Mongoose",
+    "Handlebars",
+    "Bootstrap",
+    "Materialize",
+    "MaterialUI",
+    "React-Bootstrap",
+    "Linux",
+    "Git",
+    "HTML",
+    "CSS",
+  ],
+  projects: {
+    intro: `В моей копилке несколько проектов разной степени сложности. Какие-то из них разрабытвались в команде, используя систему контроля версий git, а какие-то самостоятельно, как например, текущий сайт-резюме.
+    С проектами можно ознакомиться на отдельной странице или в непосредственно в профиле Github.`,
+    representation: [
+      {
+        desription: ``,
+        links: [],
+      },
+      {
+        desription: ``,
+        links: [],
+      },
+      {
+        desription: ``,
+        links: [],
+      },
+      {
+        desription: ``,
+        links: [],
+      },
+      {
+        desription: ``,
+        links: [],
+      },
+      {
+        desription: ``,
+        links: [],
+      },
+    ],
+  },
+  sertificates: [{ desription: `` }],
+  expirience: "",
+  futureVision: "",
+  education: "",
+};
