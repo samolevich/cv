9f26e9a8c6042386f9e8eefc14fb8a90830b228c
Konstantin Samolevich <42073618+samolevich@users.noreply.github.com>
2022-06-30 10:56:10 +0300

    add skills info, projects info

--- src/aboutMe.js
+++ src/aboutMe.js
@@ -1,49 +1,120 @@
 export default {
-  skills: [
-    "Javascript",
-    "React",
-    "Redux (Thunk)",
-    "NodeJS",
-    "Express",
-    "MongoDB",
-    "Mongoose",
-    "Handlebars",
-    "Bootstrap",
-    "Materialize",
-    "MaterialUI",
-    "React-Bootstrap",
-    "Linux",
-    "Git",
-    "HTML",
-    "CSS",
-  ],
+  title: "Javascript разработчик MERN",
+  skills: {
+    title: "Навыки",
+    list: [
+      "Javascript",
+      "React",
+      "Redux (Thunk)",
+      "NodeJS",
+      "Express",
+      "MongoDB",
+      "Mongoose",
+      "Handlebars",
+      "Bootstrap",
+      "Materialize",
+      "MaterialUI",
+      "React-Bootstrap",
+      "Linux",
+      "Git",
+      "HTML",
+      "CSS",
+    ],
+    link: "/projects",
+  },
   projects: {
-    intro: `В моей копилке несколько проектов разной степени сложности. Какие-то из них разрабытвались в команде, используя систему контроля версий git, а какие-то самостоятельно, как например, текущий сайт-резюме.
-    С проектами можно ознакомиться на отдельной странице или в непосредственно в профиле Github.`,
-    representation: [
+    title: "Проекты",
+    explanation: `В моём распоряжении имеется несколько пет-проектов. Некоторые из них разрвбатывались самостоятельно, другие - в команде.`,
+    list: [
       {
-        desription: ``,
-        links: [],
+        title: "Fridgy",
+        desription: `Приложение - виртуальный холодильник. Основная цель: показать реализацию CRUD, REST API, разработку в команде посредством GIT, взимодействие Backend + Frontend с помощью интрументов React JS, Redux, Node JS, Express JS, Mongo DB, Mongoose, React-Bootstrap.`,
+        links: [
+          {
+            name: "Heroku Deploy",
+            path: "https://fridgy-2020.herokuapp.com/",
+          },
+          {
+            name: "Основной репозиторий проекта",
+            path: "https://github.com/negomi-e/fridgy/",
+          },
+          {
+            name: "Резервный форк",
+            path: "https://github.com/samolevich/fridgy",
+          },
+        ],
       },
       {
-        desription: ``,
-        links: [],
+        title: "EventPlanner",
+        desription: `Командная реализация SSR-приложения. Добавить мероприятие, выбрать время, получить статистику.\nСтек: Node JS, Express JS, Handlebars, Mongo DB, Mongoose, BCrypt(lib), Javascript, HTML, CSS`,
+        links: [
+          {
+            name: "Основной репозиторий проекта",
+            path: "https://github.com/ArtShilov/eventPlaner",
+          },
+          {
+            name: "Резервный форк",
+            path: "https://github.com/samolevich/eventPlaner",
+          },
+        ],
       },
       {
-        desription: ``,
-        links: [],
+        title: "RentApp",
+        desription: `Командная реализация SSR-приложения. CRUD, роли пользователей (модератор, пользователь)`,
+        links: [
+          {
+            name: "Основной репозиторий проекта",
+            path: "https://github.com/SlavaPush/core-rent-app",
+          },
+          {
+            name: "Резервный форк",
+            path: "https://github.com/samolevich/core-rent-app",
+          },
+        ],
       },
       {
-        desription: ``,
-        links: [],
+        title: "API Github",
+        desription: `Реализация работы с API Github. Выдаёт последнюю активность в искомой организации (commit, pullrequest...).\n
+        Технологии: JavaScript, HTML, CSS, Materialize.`,
+        links: [
+          {
+            name: "Основной репозиторий проекта",
+            path: "https://github.com/samolevich/solo-project-api-github",
+          },
+          {
+            name: "Github Pages Deploy",
+            path: "https://samolevich.github.io/solo-project-api-github/",
+          },
+        ],
       },
       {
-        desription: ``,
-        links: [],
+        title: "API HeadHunter",
+        desription: `Проект работает с API HeadHunter и выдаёт последние опубликованные вакансии по региону Москва.\n
+        JavaScript, HTML, CSS, Materialize.`,
+        links: [
+          {
+            name: "Основной репозиторий проекта",
+            path: "https://github.com/samolevich/solo-headhunder-api",
+          },
+          {
+            name: "Github Pages Deploy",
+            path: "https://samolevich.github.io/solo-headhunder-api/",
+          },
+        ],
       },
       {
-        desription: ``,
-        links: [],
+        title: "CV",
+        desription: `React Github Profile Guide App - CV`,
+        links: [
+          {
+            name: "Основной репозиторий проекта",
+            path: "https://github.com/samolevich/cv",
+          },
+          {
+            name: "Резервный форк",
+            path: "https://samolevich.github.io/cv/",
+          },
+        ],
       },
     ],
   },
