dafc33ea9ba9cdf502dd60e24ee8da76b4e8e3fc
Konstantin Samolevich <42073618+samolevich@users.noreply.github.com>
2022-07-02 22:56:51 +0300

    skills page

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import aboutMe from "../../aboutMe";
 import style from './index.module.css';
 
 export default props => {
   return (
     <div>
       <h2>
+        <img  className={style.logo} src={aboutMe.skills.logo} alt={aboutMe.skills.title} /> {aboutMe.skills.title}
+      </h2>
+      <ul>
+        {aboutMe.skills.list.map(skill => (
+          <li className={style.skill} key={skill}>{skill}</li>
+        ))}
+      </ul>
+      {/* <h2>
         <img className={style.logo} src='img/skills.svg' alt='skills' />{' '}Навыки
       </h2>
       <ul>
         {props.skills.map(skill => (
           <li className={style.skill} key={skill}>{skill}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
