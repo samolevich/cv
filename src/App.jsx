7fdf79323ad46667976b73bd86fbc20851392c18
Konstantin Samolevich <42073618+samolevich@users.noreply.github.com>
2022-07-03 15:17:09 +0300

    throw data

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
+import aboutMe from "./aboutMe";
 import style from './App.module.css';
 import Bar from './components/Bar';
 import Info from './components/Info';
 
-const App = () => {
+export default () => {
   return (
     <div className={style.main}>
       <div></div>
       <div className={style.content}>
         <div className={style.bar}>
-          <Bar />
+          <Bar bar={aboutMe}/>
         </div>
         <div className={style.info}>
-          <Info />
+          <Info info={aboutMe}/>
         </div>
       </div>
     </div>
   );
 };
-
-export default App;
