5b08dfafd2a36a6db72bc1afedaf1804b2522760
Konstantin Samolevich <42073618+samolevich@users.noreply.github.com>
2022-07-03 15:21:38 +0300

    set navbar component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import style from './index.module.css';
-import aboutMe from "../../aboutMe";
 
-export default () => {
+export default props => {
   return (
     <nav>
-      {/* TODO Обо мне */}
       {
-        Object.values(aboutMe)
+        Object.values(props.nav)
           .filter(el => el.isNav)
           .map(el => (
             <li className={style.list} key={el.title}>
@@ -17,42 +15,7 @@ export default () => {
               </NavLink>
             </li>
           ))
-
       }
-{/*
-      <li className={style.list}>
-        <NavLink to='/' exact activeClassName={style.current}>
-          <img className={style.logo} src='img/about.svg' alt='about' /><span className={style.hiddenformobile} > Обо мне</span>
-        </NavLink>
-      </li>
-      <li className={style.list}>
-        <NavLink to='/projects' activeClassName={style.current}>
-          <img className={style.logo} src='img/projects.svg' alt='projects' /><span className={style.hiddenformobile} > Проекты</span>
-        </NavLink>
-      </li>
-      <li className={style.list}>
-        <NavLink to='/certificates' activeClassName={style.current}>
-          <img
-            className={style.logo}
-            src='img/certificate.svg'
-            alt='certificate'
-          /><span className={style.hiddenformobile}> Сертификаты</span>
-        </NavLink>
-      </li>
-      <li className={style.list}>
-        <NavLink to='/skills' activeClassName={style.current}>
-          <img className={style.logo} src='img/skills.svg' alt='skills' /><span className={style.hiddenformobile}> Навыки</span>
-        </NavLink>
-      </li>
-      <li className={style.list}>
-        <NavLink to='/expirience' activeClassName={style.current}>
-          <img
-            className={style.logo}
-            src='img/expirience.svg'
-            alt='expirience'
-          /><span className={style.hiddenformobile}> Опыт</span>
-        </NavLink>
-      </li> */}
     </nav>
   );
 };
