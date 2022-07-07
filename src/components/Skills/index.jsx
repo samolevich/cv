ebb7a5bfb3e4dad1c68e560ad61f532b0c83084c
Konstantin Samolevich <42073618+samolevich@users.noreply.github.com>
2022-07-07 14:42:12 +0300

    refactoring using props

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import aboutMe from "../../aboutMe";
 import style from './index.module.css';
 
-export default props => {
+export default ({skills: { title, logo, list }}) => {
   return (
     <div>
       <h2>
-        <img  className={style.logo} src={aboutMe.skills.logo} alt={aboutMe.skills.title} /> {aboutMe.skills.title}
+        <img  className={style.logo} src={logo} alt={title} /> {title}
       </h2>
       <ul>
-        {aboutMe.skills.list.map(skill => (
+        {list.map(skill => (
           <li className={style.skill} key={skill}>{skill}</li>
         ))}
       </ul>
