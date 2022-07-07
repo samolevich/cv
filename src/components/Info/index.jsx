c03c25ad87ea515fb3e5b0e7f5a9a2c49ee94043
Konstantin Samolevich <42073618+samolevich@users.noreply.github.com>
2022-07-07 14:14:42 +0300

    throw props

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -7,13 +7,13 @@ import Certif from '../Certif';
 import Expirience from '../Expirience';
 
 export default props => {
-  const { info: { skills: { list }, projects, expirience}} = props;
+  const { info: { skills: { list }, projects, expirience, certificates }} = props;
   return (
     <div>
       <Route exact path='/' component={() => <About skills={list} />} />
       <Route path='/skills' component={() => <Skills skills={list} />} />
       <Route path='/projects' component={() => <Projects projects={projects} />} />
-      <Route path='/certificates' component={Certif} />
+      <Route path='/certificates' component={() => <Certif certificates={certificates} />} />
       <Route path='/expirience' component={() => <Expirience exp={expirience} />} />
     </div>
   );
