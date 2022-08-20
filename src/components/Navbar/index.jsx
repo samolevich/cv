96a6da0f9d68af58c0104afb3a7d79984c9c016e
samolevich <samolevich@gmail.com>
2022-08-20 21:00:42 +0300

    fix: add ul

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,22 +5,24 @@ import style from "./index.module.css";
 export default ({ nav, lang }) => {
   return (
     <nav>
-      {Object.values(nav)
-        .filter(el => el.isNav)
-        .map(el => (
-          <li className={style.list} key={el["title" + lang]}>
-            <NavLink exact activeClassName={style.current} to={el.path}>
-              <img
-                className={style.logo}
-                src={el.logo}
-                alt={el["title" + lang]}
-              />
-              <span className={style.hiddenformobile}>
-                {el["title" + lang].toUpperCase()}
-              </span>
-            </NavLink>
-          </li>
-        ))}
+      <ul className={style.nav}>
+        {Object.values(nav)
+          .filter(el => el.isNav)
+          .map(el => (
+            <li className={style.list} key={el["title" + lang]}>
+              <NavLink exact activeClassName={style.current} to={el.path}>
+                <img
+                  className={style.logo}
+                  src={el.logo}
+                  alt={el["title" + lang]}
+                />
+                <span className={style.hiddenformobile}>
+                  {el["title" + lang].toUpperCase()}
+                </span>
+              </NavLink>
+            </li>
+          ))}
+      </ul>
     </nav>
   );
 };
